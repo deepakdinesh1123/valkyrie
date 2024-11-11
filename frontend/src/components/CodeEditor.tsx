f22eb64e484ae8511017ebae2366a05794f47042
Sujay-J-Reddy <sujayj1522002@gmail.com>
2024-11-11 18:44:56 +0530

    odin logo added

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Editor, { EditorProps } from "@monaco-editor/react";
 import { Language, LanguageVersion } from "@/api-client";
+import TabIcon from '@/assets/tabicon.svg'
 
 interface CodeEditorProps {
   selectedLanguage: Language;
   selectedLanguageVersion: LanguageVersion;
   onChange?: (content: string) => void;
   editorOptions?: EditorProps["options"];
-  value?: string; 
+  value?: string;
   height: string;
 }
 
@@ -16,7 +17,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
   onChange,
   editorOptions,
   value,
-  height, 
+  height,
 }) => {
   const handleEditorChange = (newValue: string | undefined) => {
     const newContent = newValue ?? "";
@@ -25,22 +26,24 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
 
   return (
     <div className="flex flex-col h-screen bg-[#1E1E1E] text-white">
-      <div className="flex h-14 px-4 border-b border-stone-700 pb-0 mb-0 pt-3">
+      <div className="flex h-16  border-b border-stone-700 pb-0 mb-0 pt-3">
+        <img src={TabIcon} className="ml-2" alt="Valkyrie" />
         <div
-          className="inline-block px-4 py-2 border border-stone-700 min-w-20"
-          style={{ marginBottom: '-1px' }}
+          className="inline-flex items-center px-3 py-2 border border-stone-700 min-w-20 ml-2"
+
         >
           <button className="text-sm text-white bg-transparent border-none cursor-pointer focus:outline-none pb-0 mb-0">
             {`main.${selectedLanguage.extension}`}
           </button>
         </div>
       </div>
+
       <div className="flex-grow mt-0">
         <Editor
           height={height}
           width="100%"
           language={selectedLanguage.monaco_language}
-          value={value ?? selectedLanguage.default_code} 
+          value={value ?? selectedLanguage.default_code}
           onChange={handleEditorChange}
           theme="vs-dark"
           options={{
