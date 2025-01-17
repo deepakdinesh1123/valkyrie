c5ee56390e882b3e3368b5f78e182712d036e352
deepakdinesh1123 <52698821+deepakdinesh1123@users.noreply.github.com>
2025-01-17 11:13:32 +0530

    command completion wip (#54)

--- internal/odin/cmd/root.go
+++ internal/odin/cmd/root.go
@@ -38,6 +38,7 @@ func init() {
 	RootCmd.AddCommand(worker.WorkerCmd)
 	RootCmd.AddCommand(StandaloneCmd)
 	RootCmd.AddCommand(store.StoreCmd)
+	RootCmd.AddCommand(CompletionCmd)
 
 	createDirs(envConfig)
 }
