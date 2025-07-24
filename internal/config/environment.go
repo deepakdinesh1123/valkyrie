8ac4ac450226db88ecc04bf5800138b554cdf7a2
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2025-07-24 07:30:43 +0530

    getting worker otel traces

--- internal/config/environment.go
+++ internal/config/environment.go
@@ -205,7 +205,7 @@ func setDefaults() {
 	viper.SetDefault("CPU_LIMIT", 75)
 
 	viper.SetDefault("ENABLE_TELEMETRY", false)
-	viper.SetDefault("OTLP_ENDPOINT", "localhost:4317")
+	viper.SetDefault("OTLP_ENDPOINT", "valkyrie-otel-collector:4317")
 	viper.SetDefault("OTEL_RESOURCE_NAME", "valkyrie")
 	viper.SetDefault("ENVIRONMENT", "dev")
 
